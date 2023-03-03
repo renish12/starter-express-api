d266d8babb8f09c5d6da7ceee1ea2bc64bc57c7b
Renish Radadiya <renish.megaminds@gmail.com>
2023-03-03 15:08:27 +0530

    commite

--- index.js
+++ index.js
@@ -1,7 +1,38 @@
-const express = require('express')
-const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
-})
-app.listen(process.env.PORT || 3000)
+// const express = require('express')
+// const bodyParser = require("body-parser");
+// const mongoose = require("mongoose");
+// const graphqlHttp = require("express-graphql");
+
+// const app = express()
+// app.all('/', (req, res) => {
+//     console.log("Just got a request!")
+//     res.send('Yo!')
+// })
+// app.listen(process.env.PORT || 3000)
+
+const express = require("express");
+const express_graphql = require("express-graphql");
+const { buildSchema } = require("graphql");
+// const GraphQL schema
+const schema = buildSchema(`
+    type Query {
+        message: String
+    }
+`);
+// Root resolver
+const root = {
+  message: () => "Hello World!",
+};
+// Create an express server and a GraphQL endpoint
+const app = express();
+app.use(
+  "/graphql",
+  express_graphql({
+    schema: schema,
+    rootValue: root,
+    graphiql: true,
+  })
+);
+app.listen(4000, () =>
+  console.log("`Express GraphQL Server Now Running On  http://localhost:4000 ")
+);
